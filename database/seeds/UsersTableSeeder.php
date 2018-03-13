dc07edbb733026e9be6d8e0afd01367fb6e088c4
ruiqi <rs858@njit.edu>
2018-03-13 12:52:40 -0400

    Register test made

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\User::class, 50)->create()->each(function ($u) {
+            //$u->posts()->save(factory(App\Post::class)->make());
+        });
     }
 }
